0be532cfe43f272eeb5baf8d7cbcbd8419113a06
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-07-18 15:00:45 +0530

    Add files via upload

---
+++ Assignment - 9/Assignment - 9/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message)
+        {
+        }
+    }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                try
+                {
+                    Console.WriteLine("** welcome to User Registration System **");
+
+                    Console.Write("Enter your Name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Enter your E-mail: ");
+                    string email = Console.ReadLine();
+
+                    Console.Write("Enter your Password: ");
+                    string password = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(name))
+                    {
+                        throw new ValidationException("Name is required.");
+                    }
+
+                    if (string.IsNullOrEmpty(email))
+                    {
+                        throw new ValidationException("Email is required.");
+                    }
+
+                    if (string.IsNullOrEmpty(password))
+                    {
+                        throw new ValidationException("Password is required.");
+                    }
+
+                    if (name.Length < 6)
+                    {
+                        throw new ValidationException("Name must have at least 6 characters.");
+                    }
+
+                    if (password.Length < 8)
+                    {
+                        throw new ValidationException("Password must have at least 8 characters.");
+                    }
+
+                    Console.WriteLine("User registration success.");
+                    Console.WriteLine("Name: " + name);
+                    Console.WriteLine("Email: " + email);
+                    Console.WriteLine("Password: " + password);
+                }
+                catch (ValidationException ex)
+                {
+                    Console.WriteLine("Validation error: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
